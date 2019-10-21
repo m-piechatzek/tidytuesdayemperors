a5e5f33e3a62131c5e61f4ff4089b97ae3338edd
MonikaP <piechatzek@hotmail.com>
2019-10-20 21:02:49 -0700

    killer labels

--- emperorRfile.R
+++ emperorRfile.R
@@ -5,3 +5,12 @@ library(tidyverse)
 
 # Dot plot for rise and birth_prv
 ggplot(emperors, aes(rise, birth_prv)) + geom_count()
+
+# bar chart for rise
+ggplot(emperors, aes(rise)) + geom_bar()
+
+# killer label for cause and reign_end
+ggplot(emperors, aes(cause, reign_end, "year", colour = killer)) + geom_point() + theme(legend.position = "bottom")
+
+# killer label for cause and reign_end with color
+ggplot(emperors, aes(cause, reign_end, "year", color= killer, label=emperors$killer)) + geom_point() + geom_text(aes(label = emperors$killer), hjust=0, vjust=0) + theme(legend.position = "bottom")
