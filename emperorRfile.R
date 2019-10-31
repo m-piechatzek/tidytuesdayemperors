05ae759021e96cbb1e20a2b6c85cc17a6c3781d3
MonikaP <piechatzek@hotmail.com>
2019-10-30 18:51:29 -0700

    Adding markdown and piechart

--- emperorRfile.R
+++ emperorRfile.R
@@ -15,5 +15,21 @@ ggplot(emperors, aes(cause, reign_end, "year", colour = killer)) + geom_point() 
 # killer label for cause and reign_end with color
 ggplot(emperors, aes(cause, reign_end, "year", color= killer, label=emperors$killer)) + geom_point() + geom_text(aes(label = emperors$killer), hjust=0, vjust=0) + theme(legend.position = "bottom")
 
-#calculate age
-ages <- age_calc(emperors$birth, enddate=emperors$death, units = "years", precise = TRUE)
+#calculate age (doesn't work because of BC AD years)
+ages <- age_calc(emperors$birth, enddate=emperors$death, units = "years", precise = TRUE) # dont think it works
+emperors$birthage <- format(as.Date(emperors$birth, format="%d/%m/%Y"),"%Y")
+emperors$deathage <- format(as.Date(emperors$death, format="%d/%m/%Y"),"%Y")
+emperors$birthage <- as.numeric(emperors$birthage)
+emperors$deathage <- as.numeric(emperors$deathage)
+emperors$age <- ifelse(emperors$birthage > emperors$deathage,  emperors$birthage + emperors$deathage,  emperors$deathage - emperors$birthage)
+
+
+# bar graph for ages and name
+emperors %>% filter(emperors$age < 99) %>% ggplot(emperors, mapping = aes(y = age, x = name)) + geom_bar(stat = "identity")
+
+# bar graph with age and name and colored with province and ordered
+emperors %>% filter(emperors$age < 99) %>% ggplot(emperors, mapping = aes(y = age, x = reorder(name, -age), color= birth_prv)) + geom_bar(stat = "identity")
+
+# bar graph with birth_prv only
+ggplot(emperors, aes(birth_prv)) + geom_bar()
+
